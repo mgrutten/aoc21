abc29ed77839b9b61afb1e2cd591c0af6d44af89
Mark Rutten <42272613+mgrutten@users.noreply.github.com.>
2024-12-11 21:42:42 +1030

    Day 3.

--- src/bin/day03.rs
+++ src/bin/day03.rs
@@ -1,27 +1,80 @@
 use std::error::Error;
 use std::fs;
-use array2d::Array2D;
 
-fn part1(digits: &Array2D<u8>) {
+fn decimal_values(binary: &Vec<u8>) -> (u64, u64) {
+    let mut val = 0;
+    let mut inverted = 0;
+    for (i, &bit) in binary.iter().rev().enumerate() {
+        if bit == 1 {
+            val += 2_u64.pow(i as u32);
+        } else {
+            inverted += 2_u64.pow(i as u32);
+        }
+    }
+    (val, inverted)
+}
 
-    let digit_count = digits.num_rows();
 
-    let gamma_rate_binary = digits.columns_iter()
-        .map(|c| c.fold(0_usize, |acc, v| acc + *v as usize))
-        .map(|v| if v > digit_count / 2 {1} else {0})
+fn part1(numbers: &Vec<Vec<u8>>) {
+    let number_count = numbers.len();
+    let digit_count = numbers[0].len();
+
+    let gamma_rate_binary = numbers.iter()
+        .fold(vec![0_usize; digit_count],
+              |acc, v| acc.iter().zip(v.iter())
+                  .map(|(a, b)| a + *b as usize)
+                  .collect()).iter()
+        .map(|v| if *v > number_count / 2 { 1 } else { 0 })
         .collect::<Vec<u8>>();
 
-    let mut gamma_rate = 0;
-    let mut epsilon_rate = 0;
-    for (i, &bit) in gamma_rate_binary.iter().rev().enumerate() {
-        if bit == 1 {
-            gamma_rate += 2_u64.pow(i as u32);
-        } else {
-            epsilon_rate += 2_u64.pow(i as u32);
+    let (gamma_rate, epsilon_rate) = decimal_values(&gamma_rate_binary);
+
+    println!("Part 1: {}", gamma_rate * epsilon_rate);
+}
+
+fn part2(numbers: &Vec<Vec<u8>>) {
+    let digit_count = numbers[0].len();
+
+    let mut o2_numbers = numbers.clone();
+    for col in 0..digit_count {
+        let number_count = o2_numbers.len();
+
+        let bit_count = o2_numbers.iter()
+            .map(|n| n[col] as usize)
+            .sum::<usize>();
+
+        let bit_keep = if bit_count * 2 >= number_count { 1 } else { 0 };
+        o2_numbers = o2_numbers.into_iter()
+            .filter(|n| n[col] == bit_keep)
+            .collect::<Vec<Vec<u8>>>();
+
+        if o2_numbers.len() == 1 {
+            break;
         }
     }
 
-    println!("Part 1: {}", gamma_rate * epsilon_rate);
+    let mut co2_numbers = numbers.clone();
+    for col in 0..digit_count {
+        let number_count = co2_numbers.len();
+
+        let bit_count = co2_numbers.iter()
+            .map(|n| n[col] as usize)
+            .sum::<usize>();
+
+        let bit_keep = if bit_count * 2 >= number_count { 0 } else { 1 };
+        co2_numbers = co2_numbers.into_iter()
+            .filter(|n| n[col] == bit_keep)
+            .collect::<Vec<Vec<u8>>>();
+
+        if co2_numbers.len() == 1 {
+            break;
+        }
+    }
+
+    let o2_rating = decimal_values(&o2_numbers[0]).0;
+    let co2_rating = decimal_values(&co2_numbers[0]).0;
+
+    println!("Part 2: {}", o2_rating * co2_rating);
 }
 
 
@@ -29,13 +82,12 @@ fn main() -> Result<(), Box<dyn Error>> {
     // Read in example
     let file_str: String = fs::read_to_string("data/day03/day03.txt")?;
 
-    let map_vec = file_str.lines()
+    let numbers = file_str.lines()
         .map(|line| line.bytes().map(|b| b - b'0').collect())
         .collect::<Vec<Vec<u8>>>();
 
-    let digits = Array2D::from_rows(&map_vec).unwrap();
-
-    part1(&digits);
+    part1(&numbers);
+    part2(&numbers);
 
     Ok(())
 }
