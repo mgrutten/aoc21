001829bcbd5f64b117add0be32693e662d279665
Mark Rutten <42272613+mgrutten@users.noreply.github.com.>
2024-12-14 21:52:58 +1030

    Day 10.

--- src/bin/day10.rs
+++ src/bin/day10.rs
@@ -5,12 +5,12 @@ use std::fs;
 enum ParseResult {
     OK(usize),
     Error(char),
-    Incomplete,
+    Incomplete(Vec<char>),
 }
 
 fn process_chunk(line: &Vec<char>, index: usize, closing: &HashMap<char, char>) -> ParseResult {
-    if index >= line.len() - 1 {
-        return ParseResult::Incomplete;
+    if index + 1 >= line.len() {
+        return ParseResult::Incomplete(vec![closing[&line[index]]]);
     }
 
     let mut end_index = index + 1;
@@ -18,11 +18,15 @@ fn process_chunk(line: &Vec<char>, index: usize, closing: &HashMap<char, char>) 
         match process_chunk(line, end_index, closing) {
             ParseResult::OK(process_index) => end_index = process_index,
             ParseResult::Error(bad_terminator) => return ParseResult::Error(bad_terminator),
-            ParseResult::Incomplete => return ParseResult::Incomplete,
+            ParseResult::Incomplete(terminators) => {
+                let mut new_terminators = terminators.clone();
+                new_terminators.push(closing[&line[index]]);
+                return ParseResult::Incomplete(new_terminators);
+            }
         }
 
         if end_index >= line.len() {
-            return ParseResult::Incomplete;
+            return ParseResult::Incomplete(vec![closing[&line[index]]]);
         }
     }
 
@@ -31,6 +35,20 @@ fn process_chunk(line: &Vec<char>, index: usize, closing: &HashMap<char, char>) 
     } else {
         ParseResult::Error(line[end_index])
     }
+}
+
+
+fn process_chunks(line: &Vec<char>, closing: &HashMap<char, char>) -> ParseResult {
+    let mut end_index = 0;
+    while closing.contains_key(&line[end_index]) {
+        match process_chunk(line, end_index, closing) {
+            ParseResult::OK(process_index) => end_index = process_index,
+            ParseResult::Error(bad_terminator) => return ParseResult::Error(bad_terminator),
+            ParseResult::Incomplete(terminators) => return ParseResult::Incomplete(terminators),
+        }
+    }
+
+    ParseResult::OK(0)
 }
 
 
@@ -51,17 +69,46 @@ fn part1(lines: &Vec<Vec<char>>) {
 
     let mut error_score = 0;
     for line in lines {
-        match process_chunk(line, 0, &closing_bracket) {
-            ParseResult::OK(_) => println!("complete"),
-            ParseResult::Error(bad_terminator) => {
-                error_score += scores[&bad_terminator];
-                println!("problem at {}", bad_terminator);
-            },
-            ParseResult::Incomplete => println!("incomplete"),
+        match process_chunks(line, &closing_bracket) {
+            ParseResult::OK(_) => {}
+            ParseResult::Error(bad_terminator) => error_score += scores[&bad_terminator],
+            ParseResult::Incomplete(_) => {}
         }
     }
-    
+
     println!("Part 1: {}", error_score);
+}
+
+
+fn part2(lines: &Vec<Vec<char>>) {
+    let closing_bracket = HashMap::from([
+        ('(', ')'),
+        ('[', ']'),
+        ('{', '}'),
+        ('<', '>'),
+    ]);
+
+    let scores = HashMap::from([
+        (')', 1_u64),
+        (']', 2),
+        ('}', 3),
+        ('>', 4),
+    ]);
+
+    let mut error_scores = Vec::new();
+    for line in lines {
+        match process_chunks(line, &closing_bracket) {
+            ParseResult::OK(_) => {}
+            ParseResult::Error(_) => {}
+            ParseResult::Incomplete(terminators) => {
+                let error_score = terminators.iter().fold(0, |acc, c| acc * 5 + scores[c]);
+                error_scores.push(error_score);
+            }
+        }
+    }
+
+    error_scores.sort();
+    println!("Part 2: {}", error_scores[error_scores.len() / 2]);
 }
 
 
@@ -75,6 +122,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     }
 
     part1(&lines);
+    part2(&lines);
 
     Ok(())
 }
